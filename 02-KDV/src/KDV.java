93db56ff167eb42e7ddcfa0c54e7b6e8d439dd71
Okan Arık <89224500+OkanArik@users.noreply.github.com>
2022-03-18 15:04:07 +0200

    Update KDV.java

--- 02-KDV/src/KDV.java
+++ 02-KDV/src/KDV.java
@@ -7,14 +7,25 @@ public class KDV {
         // Nor: Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18, tutar 1000'den büyük ise KDV oranını %8 olarak hesaplayın.
 
             Scanner input = new Scanner(System.in); //Programımıza nesnesini kullanarak veri girişi yapabilmek amacıyla Scanner sınıfının nesnesini yarattık.
+
+            double kdvSizFiyat, kdvLiFiyat,kdvFiyat,kdvOranı;//Kullanılacağım değişkenleri tanımladım.
+
             System.out.print("Ürünün KDV'siz fiyatını giriniz:");
-            double kdvSizFiyat = input.nextDouble();
+            kdvSizFiyat = input.nextDouble();
+
             String result=kdvSizFiyat<0 ? "Geçersiz fiyat girdiniz!": "İşlemler gerçekleştiriliyor:";
-            if(kdvSizFiyat>=0) { // Buradadaki kontrolümle eğer geçersiz değer girilirse işlemleri yapmıcam.
+            System.out.println((result));
+
+            if(kdvSizFiyat>=0) {
+                kdvOranı=kdvSizFiyat<1000?0.18:0.08;
+                System.out.println("KDV oranı    :"+kdvOranı);
+
                 System.out.println("KDV'siz tutar: " + kdvSizFiyat);
-                double kdvLiFiyat = kdvSizFiyat < 1000 ? (((kdvSizFiyat / 100) * 18) + kdvSizFiyat) : (((kdvSizFiyat / 100) * 8) + kdvSizFiyat);
+
+                kdvLiFiyat = kdvSizFiyat * kdvOranı + kdvSizFiyat;
                 System.out.println("KDV'li tutar : " + kdvLiFiyat);
-                double kdvFiyat = kdvSizFiyat < 1000 ? (((kdvSizFiyat) / 100) * 18) : (((kdvSizFiyat) / 100) * 8);
+
+                kdvFiyat = kdvSizFiyat * kdvOranı;
                 System.out.println("KDV tutarı   : " + kdvFiyat);
             }
     }
