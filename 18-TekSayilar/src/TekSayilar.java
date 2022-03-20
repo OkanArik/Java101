d2c665526b03ad827af1c95f35cfb5a7531bf380
Okan Arık <89224500+OkanArik@users.noreply.github.com>
2022-03-20 20:33:44 +0300

    Add files via upload

---
+++ 18-TekSayilar/src/TekSayilar.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class TekSayilar {
+
+    public static void tekSayilar(int sayi)
+    {
+        System.out.println("Giriş yaptığınız sayıya kadar olan pozitif tek sayılar:");
+        for (int i =1 ; i<=sayi ; i+=2)
+        {
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+    }
+
+    public static int ciftIseToplaTekIseSonlandir(int sayi) {
+        if (sayi % 2 == 0)
+        {
+            return sayi;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static void main (String[] args) {
+        int sayi,sonuc=0;
+        boolean kontrol= true;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı giriniz : ");
+        sayi = input.nextInt();
+
+        tekSayilar(sayi);
+
+        System.out.println("Gireceğin çift sayıları toplayalım sonucu verelim, sonlandırmak için herhangi bir tek sayi girmen yeterli :");
+        while(true)
+        {
+            System.out.print("Sayı giriniz : ");
+            sayi =input.nextInt();
+            if(sayi%2==1)
+                break;
+            sonuc += ciftIseToplaTekIseSonlandir(sayi);
+        }
+        System.out.println("Girdiğiniz çift sayıların toplamı : "+sonuc);
+    }
+}
+
+
