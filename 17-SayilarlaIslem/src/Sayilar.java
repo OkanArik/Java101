2856ddb8ff95df7ff3c9ac5bb8609f48d3f5f1b5
Okan Arık <89224500+OkanArik@users.noreply.github.com>
2022-03-20 20:09:29 +0300

    Add files via upload

---
+++ 17-SayilarlaIslem/src/Sayilar.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Sayilar {
+
+    public static void ciftSayilar(int sayi)
+    {
+        System.out.println("Giriş yaptığınız sayıya kadar olan pozitif çif sayılar:");
+        for (int i =2 ; i<=sayi ; i+=2)
+        {
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+    }
+
+    public static void ucVeDortTamBolunenler(int sayi)
+    {
+        System.out.println("Giriş yaptınız sayıya kadar olan sayılardan 3'e tam bölünen pozitif sayılar:");
+        for (int i =3; i<=sayi ; i+=3)
+        {
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+
+        System.out.println("Giriş yaptınız sayıya kadar olan sayılardan 3'e tam bölünen pozitif sayılar:");
+        for (int i =4; i<=sayi ; i+=4)
+        {
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+    }
+
+    public static void main (String[] args) {
+        int sayi;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı giriniz : ");
+        sayi = input.nextInt();
+
+        ciftSayilar(sayi);
+        ucVeDortTamBolunenler(sayi);
+    }
+}
+
+
