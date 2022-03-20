256f5677744774034b3752b8b681b8549b7c81fb
Okan Arık <89224500+OkanArik@users.noreply.github.com>
2022-03-20 19:16:43 +0200

    Update Sayilar.java

--- 17-SayilarlaIslem/src/Sayilar.java
+++ 17-SayilarlaIslem/src/Sayilar.java
@@ -4,7 +4,7 @@ public class Sayilar {
 
     public static void ciftSayilar(int sayi)
     {
-        System.out.println("Giriş yaptığınız sayıya kadar olan pozitif çif sayılar:");
+        System.out.println("Giriş yaptığınız sayıya kadar olan pozitif çift sayılar:");
         for (int i =2 ; i<=sayi ; i+=2)
         {
             System.out.print(i+"\t");
@@ -21,7 +21,7 @@ public class Sayilar {
         }
         System.out.println();
 
-        System.out.println("Giriş yaptınız sayıya kadar olan sayılardan 3'e tam bölünen pozitif sayılar:");
+        System.out.println("Giriş yaptınız sayıya kadar olan sayılardan 4'e tam bölünen pozitif sayılar:");
         for (int i =4; i<=sayi ; i+=4)
         {
             System.out.print(i+"\t");
