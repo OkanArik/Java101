f9a19bd9ccb8e7549de94e671082f46e971078b6
Okan Arık <89224500+OkanArik@users.noreply.github.com>
2022-04-06 15:43:22 +0300

    Update MaxAndMinNumber.java

--- 27-MaxAndMinnumber/src/MaxAndMinNumber.java
+++ 27-MaxAndMinnumber/src/MaxAndMinNumber.java
@@ -1,33 +1,66 @@
 import java.util.Scanner;
 
 public class MaxAndMinNumber {
+    public static int maxNumberOfNumberArray(int[] numbers,int n)
+    {
+        for (int i= 0 ; i<n ; i++)
+        {
+            numbers[0] = numbers[0] > numbers[i]? numbers[0]:numbers[i];
+        }
+        return numbers[0];
+    }
+
+    public static int minNumberOfNumberArray(int[] numbers,int n)
+    {
+        for (int i= 0 ; i<n ; i++)
+        {
+            numbers[0] = numbers[0] < numbers[i]? numbers[0]:numbers[i];
+        }
+        return numbers[0];
+    }
 
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
-        int n , maxNumber=0 , minNumber=0;
+        int n , maxNumber , minNumber;
 
-        System.out.print("En büyük ve en küçüklerini bulmak için kaç adet sayıyı karşılastıracaksınız : ");
-        n=input.nextInt();
-
-        int[] numberArray= new int[n];
-
-        System.out.println("Sayıları giriniz : ");
-        for(int i = 0 ; i<n ; i++)
+        while(true)
         {
-            System.out.print(i+1 +" Sayıyı giriniz : " );
-            numberArray[i]=input.nextInt();
-            maxNumber = numberArray[i]>maxNumber? numberArray[i]: maxNumber;
-            minNumber = numberArray[i]<minNumber? numberArray[i]: minNumber;
-        }
-        System.out.println("En büyük sayi : "+maxNumber);
-        System.out.println("En küçük sayi : "+minNumber);
-         System.out.print("Girdiğiniz sayilar : ");
-        for(int i =0 ; i<n ; i++)
-        {
-            System.out.print("\t"+numberArray[i]);
+            System.out.println("En büyük ve en küçüklerini bulmak için kaç adet sayıyı karşılastıracaksınız : ");
+            System.out.println("Çıkış yapmak istiyorsanız 0 giriniz:");
+
+            n=input.nextInt();
+            if(n<0)
+            {
+                System.out.println("Geçersiz değer girdiniz tekrar deneyiniz!");
+                continue;
+            }
+            else if(n==0)
+            {
+                System.out.println("Güle Güle");
+                break;
+            }
+
+            int[] numberArray= new int[n];
+
+            System.out.println("Sayıları giriniz : ");
+            for(int i = 0 ; i<n ; i++)
+            {
+                System.out.print(i+1 +" Sayıyı giriniz : " );
+                numberArray[i]=input.nextInt();
+            }
+            maxNumber=maxNumberOfNumberArray(numberArray,n);
+            minNumber=minNumberOfNumberArray(numberArray,n);
+            System.out.println("En büyük sayi : "+maxNumber);
+            System.out.println("En küçük sayi : "+minNumber);
+            System.out.print("Girdiğiniz sayilar : ");
+            for(int i =0 ; i<n ; i++)
+            {
+                System.out.print("\t"+numberArray[i]);
+            }
+            System.out.println();
         }
     }
-
 }
+
 
 
